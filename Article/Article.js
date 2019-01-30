12cf8fca400fd387f0e7ec395f4a7caebea198d9
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-30 17:07:06 -0500

    pt2 Komplete

--- Article/Article.js
+++ Article/Article.js
@@ -3,17 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.addEventListener("click", () => this.expandArticle());
+}
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle('article-open')
+    if (this.expandButton.textContent = 'Close'{
+      this.expandButton.textContent = 'Expand'
+    }
+    else{
+      this.expandButton.textContent = 'Close'
+    }
 
   }
 }
@@ -26,4 +32,5 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(.articles);
+articles.forEach(article => new Article(article));
